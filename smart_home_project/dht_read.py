0b8d1b1dab738d195d2a1d1c167b15d01619563d
HottJ <jhj13062004@naver.com>
2019-11-27 21:15:20 +0900

    Update dht_read.py

--- smart_home_project/dht_read.py
+++ smart_home_project/dht_read.py
@@ -10,12 +10,16 @@ data=DHTData()
 data.define_sensor('DHT2',Adafruit_DHT.DHT22,2)
 
 Red=11
+yellow=29
+green=31
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 
 GPIO.setup(Red,GPIO.OUT)
 GPIO.output(Red,0)
+GPIO.output(yellow,0)
+GPIO.output(green,0)
 
 humidity,temperature=Adafruit_DHT.read_retry(22,2)
 
@@ -31,9 +35,9 @@ try:
                     GPIO.output(Red,1)
             
                 elif temperature <=17:
-                    GPIO.output(Red,1)
+                    GPIO.output(green,1)
                 else:
-                    GPIO.output(Red,1)
+                    GPIO.output(yellow,1)
             data.add_reading(reading_time,'{0} Humidity'.format(sensor.name),humidity)
             data.add_reading(reading_time,'{0} temperature'.format(sensor.name),temperature)
         time.sleep(2.0)
